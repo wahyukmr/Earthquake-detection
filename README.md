5678c8836298a2c94302d1b3a429e4c2f28885e0
wahyu-hidayah <maryu2992@gmail.com>
2022-03-18 16:14:08 +0700

    release to pypi at https://pypi.org/project/Earthquakedetection/0.0.2/

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
+
 # Earthquake-detection
-This package will detect the latest earthquakes from BMKG | Meteorology, Climatology, and Geophysical Agency
+>This package will detect the latest earthquakes from **BMKG** *(Meteorology, Climatology, and Geophysical Agency)*.<br>
+>You can use it to find out the latest earthquake news.
+
+[![map-of-indonesia.png](https://i.postimg.cc/QNFYhPVJ/map-of-indonesia.png)](https://postimg.cc/14ZrKvG8)
+
+### How it work
+- This package will retrieve data from [BMKG](https://bmkg.go.id) to get the latest earthquake news in Indonesia.
+- This package uses Beautifulsoup4 and requests.
+- and will produce an output in the form of JSON that is ready to be used in web or mobile applications.
+
+### How to use
+~~~
+if __name__ == '__main__':
+    print('aplikasi utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
+~~~
+
+### Author
+wahyukmr
---
+++ setup.py
@@ -0,0 +1,34 @@
+"""
+Packaging Python Projects: https://packaging.python.org/en/latest/tutorials/packaging-projects/
+markdown guide: https://www.markdownguide.org/cheat-sheet/
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Earthquakedetection",
+    version="0.0.2",
+    author="wahyukmr",
+    author_email="maryu9292@gmail.com",
+    description="This package will detect the latest earthquakes from BMKG (Meteorology, Climatology, and "
+                "Geophysical Agency)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wahyukmr/Earthquake-detection",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Framework :: Scrapy",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
